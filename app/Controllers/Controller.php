400293b658b1d5fc933b76251b0b41de00514471
baazz20 <yaoeloge@outlook.fr>
2021-11-11 10:03:37 +0100

    Mise en place du systeme de vue

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -6,6 +6,13 @@ class Controller{
 
     public  function view(string $path, array $params = null)
     {
-        # code...
+       ob_start();
+       $path = str_replace('.', DIRECTORY_SEPARATOR, $path);
+       require VIEWS . $path . '.php';
+       if ($params) {
+           $params = extract($params);
+       }
+       $content = ob_get_clean();
+       require VIEWS . 'layout.php';
     }
 }
